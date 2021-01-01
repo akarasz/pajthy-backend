2a15e7ee19e17c58595abe54daa4473c1b4b17e3
Andras Karasz <andras.karasz@logmein.com>
2021-01-01 14:53:40 +0100

    test: use testify for store

--- store/store_test.go
+++ store/store_test.go
@@ -3,6 +3,9 @@ package store_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/akarasz/pajthy-backend/domain"
 	"github.com/akarasz/pajthy-backend/store"
 )
@@ -12,29 +15,19 @@ func TestLoad(t *testing.T) {
 
 	// loading a non-existent session should return an error
 	_, err := s.Load("id")
-	if want := store.ErrNotExists; err != want {
-		t.Errorf("wrong error when loading non-existent session: %v", err)
-	}
+	assert.Equal(t, err, store.ErrNotExists)
 
 	// loading a non-locked id should return an error
 	created := domain.NewSession()
-	if err := s.Create("id", created); err != nil {
-		t.Fatalf("failed to create session: %v", err)
-	}
+	require.NoError(t, s.Create("id", created))
 
 	_, err = s.Load("id")
-	if want := store.ErrNotLocked; err != want {
-		t.Errorf("wrong error when loading non-locked session: %v", err)
-	}
+	assert.Equal(t, err, store.ErrNotLocked)
 
 	// loading should return the session
 	lock(t, s, "id")
-	got, err := s.Load("id")
-	if err != nil {
-		t.Fatalf("unexpected error when loading: %v", err)
-	}
-	if want := created; got != want {
-		t.Errorf("wrong session returned. got %v want %v", got, want)
+	if got, err := s.Load("id"); assert.NoError(t, err) {
+		assert.Exactly(t, created, got)
 	}
 	unlock(t, s, "id")
 }
@@ -44,67 +37,48 @@ func TestCreate(t *testing.T) {
 
 	// created session should be returned by load
 	want := domain.NewSession()
-	if err := s.Create("id", want); err != nil {
-		t.Errorf("failed to create session: %v", err)
-	}
+	assert.NoError(t, s.Create("id", want))
 
 	lock(t, s, "id")
 	got, err := s.Load("id")
-	if err != nil {
-		t.Fatalf("failed to load session just created: %v", err)
-	}
-	if got != want {
-		t.Errorf("not the same session loaded that go created. got %v want %v", got, want)
-	}
+	require.NoError(t, err)
+	assert.Exactly(t, want, got)
 	unlock(t, s, "id")
 
 	// creating with an existing id should return an error
-	if err := s.Create("id", want); err != store.ErrAlreadyExists {
-		t.Errorf("wrong error when creating with already existing id: %v", err)
-	}
+	assert.Equal(t, s.Create("id", want), store.ErrAlreadyExists)
 }
 
 func TestUpdate(t *testing.T) {
 	s := store.New()
 
 	// update non existing id should return error
-	if err := s.Update("id", domain.NewSession()); err != store.ErrNotExists {
-		t.Errorf("wrong error when trying to update non-existent session: %v", err)
-	}
+	assert.Equal(t, store.ErrNotExists, s.Update("id", domain.NewSession()))
 
 	// update existing without locking should return error
 	created := domain.NewSession()
-	if err := s.Create("id", created); err != nil {
-		t.Fatalf("failed to create session: %v", err)
-	}
-	if err := s.Update("id", domain.NewSession()); err != store.ErrNotLocked {
-		t.Errorf("wrong error when updating without locking: %v", err)
-	}
+	require.NoError(t, s.Create("id", created))
+
+	assert.Equal(t, store.ErrNotLocked, s.Update("id", domain.NewSession()))
 
 	// loading after updating right should return the same as updated
 	lock(t, s, "id")
+
 	updated := domain.NewSession()
-	if err := s.Update("id", updated); err != nil {
-		t.Fatalf("unexpected error while updating: %v", err)
-	}
+	require.NoError(t, s.Update("id", updated))
+
 	loaded, err := s.Load("id")
-	if err != nil {
-		t.Fatalf("unable to load just updated session: %v", err)
-	}
-	if loaded != updated {
-		t.Errorf("not the updated session was loaded. got %v want %v", loaded, updated)
-	}
+	require.NoError(t, err)
+
+	assert.Exactly(t, loaded, updated)
+
 	unlock(t, s, "id")
 }
 
 func lock(t *testing.T, s *store.Store, id string) {
-	if err := s.Lock(id); err != nil {
-		t.Fatalf("unexpected error while locking: %v", err)
-	}
+	require.NoError(t, s.Lock(id))
 }
 
 func unlock(t *testing.T, s *store.Store, id string) {
-	if err := s.Unlock(id); err != nil {
-		t.Fatalf("unexpected error while unlocking: %v", err)
-	}
+	require.NoError(t, s.Unlock(id))
 }
